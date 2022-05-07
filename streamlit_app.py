777846489b7531dd2ebef969ac0b6cd4e0eaea5e
ApoorvShukla99 <104310838+ApoorvShukla99@users.noreply.github.com>
2022-05-08 01:52:53 +0530

    Update streamlit_app.py
    indentation changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,21 +48,14 @@ streamlit.header("The fruit load list contains:")
 # SNowflake related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("SELECT * from fruit_load_list;")
+    my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
-  
-  # Add a button to load the fruit
-  if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["Snowflake"])
-    my_data_row = get_fruit_load_list()
-    streamlit.dataframe(my_data_rows)
-  
-#Stop anything beyond this wile we troubleshoot
-streamlit.stop()
-
-#connecting with snowflake.
-# import snowflake.connector
-
+ 
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["Snowflake"])
+  my_data_row = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
@@ -75,10 +68,3 @@ if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["Snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
-  
-
-
-
-
-# Display the table on the page
-#streamlit.dataframe(my_fruit_list)
